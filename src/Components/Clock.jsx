275d14bcb7bfe65107fd0cf9ca3876e2f6393700
KevinVeliz <mario-veliz258934@hotmail.com>
2022-11-18 22:12:15 -0500

    Reloj terminado

--- src/Components/Clock.jsx
+++ src/Components/Clock.jsx
@@ -1,12 +1,58 @@
+import { useEffect, useState } from "react";
+import "../App.css";
 const Clock = () => {
+	const [hours, setHours] = useState();
+	const [minutes, setMinutes] = useState();
+	const [seconds, setSeconds] = useState();
 
-    
+	setInterval(() => {
+		clock();
+	}, 1000);
+
+    let porcentajeHoras, porcentajeMinutos, porcentajeSegundos;
+	const clock = () => {
+		let date = new Date();
+		let second = date.getSeconds()
+		let minute =  date.getMinutes()
+        let hour = date.getHours()
+        
+        if(hour >= 12){
+            porcentajeHoras = hour / 12 * 360
+        } else {
+            porcentajeHoras = hour / 24 * 360
+
+        }
+        porcentajeHoras += minute / 60 * 30
+        porcentajeMinutos = minute / 60 * 360
+        porcentajeSegundos = second / 60 * 360
+
+		setHours(porcentajeHoras);
+		setMinutes(porcentajeMinutos);
+		setSeconds(porcentajeSegundos);
+
+
+	};
 
 	return (
 		<div className="clock">
-			<div className="hand hour" data-hour-hand></div>
-			<div className="hand minute" data-minute-hand></div>
-			<div className="hand second" data-second-hand></div>
+			<div
+				className="hand hour"
+				data-hour-hand
+				style={{
+					transform: `translate(-50%) rotate(${hours}deg)`,
+				}}></div>
+			<div
+				className="hand minute"
+				data-minute-hand
+				style={{
+					transform: `translate(-50%) rotate(${minutes}deg)`,
+				}}></div>
+			<div
+				className="hand second"
+				data-second-hand
+				style={{
+					transform: `translate(-50%) rotate(${seconds}deg)`,
+				}}></div>
 			<div className="number number1">1</div>
 			<div className="number number2">2</div>
 			<div className="number number3">3</div>
